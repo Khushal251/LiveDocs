1785aab0dcf7f6bab93e2842a4924a3fac2b0b72
Khushal Kumawat <kumawatkhushal251@gmail.com>
2024-07-21 22:45:08 +0530

    stripe added

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Documents = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Documents
---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/Header'
+import { Button } from '@/components/ui/button'
+// import { Editor } from 'jsm-editor'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+      <Header className='sticky left-0 top-0'>
+        <div>
+          hii
+          
+        </div>
+      </Header>
+    </div>
+  )
+}
+
+export default Home
