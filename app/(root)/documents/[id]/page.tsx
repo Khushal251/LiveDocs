3d91a56449e8473d1b453319483ede92b1453a71
Khushal Kumawat <kumawatkhushal251@gmail.com>
2024-07-22 23:21:36 +0530

    Added Collaborative features

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,15 +1,32 @@
 import CollaborativeRoom from '@/components/CollaborativeRoom'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { getDocument } from '@/lib/actions/room.actions'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Documents = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect('/sign-in');
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect('/');
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom/>
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+        // users={usersData}
+        // currentUserType={currentUserType}
+      />
     </main>
   )
 }
 
-export default Documents
+export default Document
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn'
 import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
+import { getDocuments } from '@/lib/actions/room.actions'
+import { dateConverter } from '@/lib/utils'
 import { SignedIn, UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 // import { Editor } from 'jsm-editor'
 import React from 'react'
@@ -12,7 +15,8 @@ const Home = async () => {
   const clerkUser = await currentUser();
   if (!clerkUser) redirect('sign-in')
 
-  const documents = [];
+
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
   return (
     <main className="home-container">
       {/* <Button>Click Me</Button> */}
@@ -25,9 +29,9 @@ const Home = async () => {
         </div>
       </Header>
 
-      {documents.length > 0 ? (
+      {roomDocuments.data.length > 0 ? (
         <div className="document-list-container">
-          {/* <div className="document-list-title">
+          <div className="document-list-title">
             <h3 className="text-28-semibold">All documents</h3>
             <AddDocumentBtn 
               userId={clerkUser.id}
@@ -51,10 +55,10 @@ const Home = async () => {
                     <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
                   </div>
                 </Link>
-                <DeleteModal roomId={id} />
+                {/* <DeleteModal roomId={id} /> */}
               </li>
             ))}
-          </ul> */}
+          </ul>
         </div>
       ) : (
         <div className="document-list-empty">
